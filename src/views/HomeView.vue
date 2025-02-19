f82d6bc44a4624b59eed48052d7091eaf99b462e
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2025-02-19 07:01:54 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import ProductCard from "@/components/ProductCard.vue";
+import Pagination from "@/components/Pagination.vue";
+</script>
+
+<template>
+  <main>
+    <div class="product-grid">
+      <ProductCard />
+    </div>
+    <div class="pagination">
+      <Pagination />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.product-grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+  gap: 20px;
+  width: 80%;
+  margin: 0 auto;
+}
+
+.pagination {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+}
+</style>
