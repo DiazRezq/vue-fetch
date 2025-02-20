5c0a04a6fdf1a8e6a74ae24ae1d76f64cb9a2265
Diaz Rezqi Firmansyah <diaz.rezqi@gmail.com>
2025-02-20 18:00:48 +0700

    lyfecycle

---
+++ src/views/AboutView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda cumque
+      minima facilis mollitia quisquam neque quos voluptas eligendi, minus
+      sapiente. Minima dolore vitae excepturi, sit nesciunt repellendus impedit
+      expedita necessitatibus.
+    </p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,44 @@
 <script setup>
 import ProductCard from "@/components/ProductCard.vue";
 import Pagination from "@/components/Pagination.vue";
+
+import {
+  onBeforeMount,
+  onBeforeUpdate,
+  onMounted,
+  onUnmounted,
+  ref,
+} from "vue";
+
+const page = ref(1);
+
+onBeforeMount(() => {
+  console.log("onBeforeMount");
+  // persiapan atau effek sebelum mounting
+});
+
+onMounted(() => {
+  console.log("onMounted");
+});
+
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate");
+  // persiapan atau effek sebelum update
+});
+
+onUnmounted(() => {
+  console.log("onUnmounted");
+});
+
+function nextPage() {
+  page.value++;
+}
 </script>
 
 <template>
   <main>
+    {{ page }}
+    <button @click="nextPage">Next</button>
     <div class="product-grid">
       <ProductCard />
     </div>
